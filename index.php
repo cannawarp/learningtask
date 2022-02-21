248113dcc5215b144a8908871fbe72f6d72199b1
cannawarp <100110974+cannawarp@users.noreply.github.com>
2022-02-20 18:43:18 -1200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to My Website! I am Ralph Nestor Lazarte!</h1>
     <ul>
       <li><a href="index.php">HOME</a></li>
-      <li><a href="https://github.com/cannawarp/learningtask/blob/main/LT2/Index.html">Learning Task</a></li>
+      <li><a href="LT2/Index.html">Learning Task</a></li>
     </ul>
   </body>
 </html>
