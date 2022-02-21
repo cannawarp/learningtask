179a34c718f90a408a1a1a2c0c1958f8e80d849c
cannawarp <100110974+cannawarp@users.noreply.github.com>
2022-02-20 17:15:01 -1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>My Home Page</title>
+    </head>
+  <body>
+    <h1>Welcome to My Website! I am Ralph Nestor Lazarte!</h1>
+    <ul>
+      <li><a href="index.php">HOME</a></li>
+    </ul>
+  </body>
+</html>
